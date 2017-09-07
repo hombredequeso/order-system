972e0d58e8edf52951f9df00e356e09ab5650699
markc <markc@pageuppeople.com>
2017-09-07 23:12:36 +1000

    Add missing file

---
+++ service-a/Statistics/AddToStatsWhenOrderPlacedHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarrierPidgin.OrderService.Messages;
+using NLog;
+
+namespace CarrierPidgin.ServiceA.Statistics
+{
+    public static class OrderStatistics
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static int TotalOrdersReceived = 0;
+        public static void OrderReceived()
+        {
+            ++TotalOrdersReceived;
+            Logger.Trace($"Orders received incremented to {TotalOrdersReceived}");
+        }
+    }
+
+    public class AddToStatsWhenOrderPlacedHandler
+    {
+        public void Handle(OrderPlacedEvent orderPlacedEvent)
+        {
+            OrderStatistics.OrderReceived();
+        }
+    }
+}
