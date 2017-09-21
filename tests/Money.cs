97b5be204c43d85cfbb861e6dab8ac273e28a36e
markc <markc@pageuppeople.com>
2017-09-21 21:46:54 +1000

    Add Money test class for wiki documentation purposes

---
+++ tests/Money.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+// https://github.com/hombredequeso/carrier-pidgin/wiki/Strong-Types
+
+namespace tests
+{
+    public enum Currency
+    {
+        AUD,
+        USD,
+        NZD
+    }
+    public class Money : IEquatable<Money>
+    {
+        public Money(Currency currency, decimal amount)
+        {
+            Currency = currency;
+            Amount = amount;
+        }
+
+        public Currency Currency { get; }
+        public Decimal Amount { get; }
+
+        public bool Equals(Money other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Currency == other.Currency && Amount == other.Amount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Money) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((int) Currency * 397) ^ Amount.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Money left, Money right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Money left, Money right)
+        {
+            return !Equals(left, right);
+        }
+
+    }
+}
